848c078ed2a5893228dde1ae089f858d4cc324da
Fabiano Fagundes Scalco <fabiano.scalco@gmail.com>
2021-10-18 20:07:39 +1000

    Refactor broadcasting to use one line for all actions

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,5 +1,7 @@
 class Tweet < ApplicationRecord
   validates :body, presence: true
 
-  after_create_commit { broadcast_prepend_to "tweets" }
+  # after_create_commit { broadcast_prepend_to "tweets" }
+  # after_destroy_commit { broadcast_remove_to "tweets" }
+  broadcasts_to ->(tweet) { :tweets }, inserts_by: :prepend, target: "tweets"
 end
