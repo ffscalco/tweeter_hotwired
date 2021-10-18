da555899d836abe15e53c5acc63219bec9a0d6fb
Fabiano Fagundes Scalco <fabiano.scalco@gmail.com>
2021-10-18 20:53:11 +1000

    Add in-place update

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -44,6 +44,8 @@ class TweetsController < ApplicationController
         format.html { redirect_to tweets_url, notice: "Tweet was successfully updated." }
         format.json { render :show, status: :ok, location: @tweet }
       else
+        format.turbo_stream { render turbo_stream: turbo_stream.replace(@tweet, partial: "tweets/form", locals: { tweet: @tweet}) }
+        @tweets = Tweet.all.reverse
         format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @tweet.errors, status: :unprocessable_entity }
       end
